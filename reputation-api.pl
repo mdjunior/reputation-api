76c49d4f2c679bbf6c2667b4b9036857c4e4678f
Manoel Domingues Junior <manoel.junior@corp.globo.com>
2015-02-10 01:10:20 -0200

    [FIX] Run perlcritic and perltidy

--- reputation-api.pl
+++ reputation-api.pl
@@ -22,47 +22,54 @@ our $VERSION = 1.0;
 
 ###############################################################################
 Readonly my $DEFAULT_VALUE => 100;
-Readonly my $EXPIRATION => 86400;
-Readonly my @collections => split / /, $ENV{REPUTATION_API_COLLECTIONS};
+Readonly my $EXPIRATION    => 86_400;
+Readonly my @COLLECTIONS   => split / /sm, $ENV{REPUTATION_API_COLLECTIONS};
 ###############################################################################
 
 ###############################################################################
 ## Helper for redis
-helper redis => sub { 
-    shift->stash->{redis} ||= Mojo::Redis2->new(url => $ENV{REPUTATION_API_REDIS_URL} );
+helper redis => sub {
+    shift->stash->{redis}
+        ||= Mojo::Redis2->new( url => $ENV{REPUTATION_API_REDIS_URL} );
 };
 
 ###############################################################################
 ## Helper for mysql
-my $dbh = DBI->connect( "DBI:mysqlPP:$ENV{REPUTATION_API_DB_NAME}:$ENV{REPUTATION_API_DB_HOST}",$ENV{REPUTATION_API_DB_USER},$ENV{REPUTATION_API_DB_PASS}) or exit;
-helper db => sub { $dbh };
+my $dbh = DBI->connect(
+    "DBI:mysqlPP:$ENV{REPUTATION_API_DB_NAME}:$ENV{REPUTATION_API_DB_HOST}",
+    $ENV{REPUTATION_API_DB_USER},
+    $ENV{REPUTATION_API_DB_PASS}
+) or exit;
+helper db => sub {$dbh};
 
 ###############################################################################
 
 get '/status' => sub {
-    my $c = shift;
+    my $c      = shift;
     my $status = 'WORKING';
 
-    #  is not necessary to check the mysql because the application does not start without it
+#  is not necessary to check the mysql because the application does not start without it
 
     #  checking redis
-    my $res = $c->redis->set(foo => "42");
-    $res = $c->redis->get("foo");
-    if ($res ne "42") {
+    my $res = $c->redis->set( foo => '42' );
+    $res = $c->redis->get('foo');
+    if ( $res ne '42' ) {
         $status = 'FAIL';
     }
 
-    $c->render(text => $status);
+    $c->render( text => $status );
 };
 
 
-app->config(hypnotoad => {
-    listen => [ 'http://*:8080' ],
-    workers => $ENV{REPUTATION_API_WORKERS},
-    clients => $ENV{REPUTATION_API_CLIENTS},
-    lock_file => $ENV{REPUTATION_API_LOCK_FILE},
-    pid_file => $ENV{REPUTATION_API_PID_FILE},
-});
+app->config(
+    hypnotoad => {
+        listen    => ['http://*:8080'],
+        workers   => $ENV{REPUTATION_API_WORKERS},
+        clients   => $ENV{REPUTATION_API_CLIENTS},
+        lock_file => $ENV{REPUTATION_API_LOCK_FILE},
+        pid_file  => $ENV{REPUTATION_API_PID_FILE},
+    }
+);
 
 app->log->level('debug');
 app->start;
